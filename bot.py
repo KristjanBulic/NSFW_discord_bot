06c495fae3772507afbeab68cccc5141817097e8
KristjanBulic <73641696+KristjanBulic@users.noreply.github.com>
2021-02-10 09:36:31 +0100

    Added hentai

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 import requests
 import random
 
+
 used = []
+
 
 def get_pic(part):
     parts = {"tits": "https://www.pornpics.com/natural-tits/",
@@ -27,9 +29,25 @@ def get_pic(part):
 
     if wanted_pic in used:
         get_pic(part)
+
     else:
         used.append(wanted_pic)
         return wanted_pic
+
+
+def get_hentai():
+    html = requests.get("https://www.pornhub.com/album/36835462").content
+    root = BeautifulSoup(html, "html.parser")
+    pics = []
+    for pic in root.find_all('div', {"class": "js_lazy_bkg photoAlbumListBlock"}):
+        picture = "https://www.pornhub.com" + pic.find('a').get('href')
+        pics.append(picture)
+
+    wanted_pic = random.choice(pics)
+    wanted_pic = BeautifulSoup(requests.get(wanted_pic).content, "html.parser")
+    hen = wanted_pic.find('div', {"class": "centerImage"}).find('img').get('src')
+    return hen
+
 
 client = commands.Bot(command_prefix=".")
 
@@ -43,44 +61,53 @@ async def on_ready():
 async def tits(ctx):
     await ctx.send(get_pic("tits"))
 
+
 @client.command()
 async def ass(ctx):
     await ctx.send(get_pic("ass"))
+
 
 @client.command()
 async def pussy(ctx):
     await ctx.send(get_pic("pussy"))
 
+
 @client.command()
 async def asian(ctx):
     await ctx.send(get_pic("asian"))
+
 
 @client.command()
 async def baklava(ctx):
     await ctx.send("Jebi se stricel")
 
+
 @client.command()
 async def burek(ctx):
-    await ctx.send("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fu9UnoW4Zr2M%2Fmaxresdefault.jpg&f=1&nofb=1")
+    await ctx.send("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fu9UnoW4Zr2M%2Fmaxres"
+                   "default.jpg&f=1&nofb=1")
+
 
 @client.command()
 async def jesus(ctx):
     await ctx.send("https://deebrestin.com/wp-content/uploads/2014/04/Jesus-Good-Shepherd-06.jpg")
 
+
 @client.command()
 async def milf(ctx):
     await ctx.send(get_pic("milf"))
 
+
 @client.command()
 async def midget(ctx):
     await ctx.send(get_pic("midget"))
-    
-    
+
+
 @client.command()
 async def creampie(ctx):
     await ctx.send(get_pic("creampie"))
-    
- 
+
+
 @client.command()
 async def supermidget(ctx):
     i = 0
@@ -91,6 +118,10 @@ async def supermidget(ctx):
         except:
             break
 
+
+@client.command()
+async def hentai(ctx):
+    await ctx.send(get_hentai())
 
 
 client.run("") #token
